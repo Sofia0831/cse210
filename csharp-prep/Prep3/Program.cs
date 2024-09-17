696f98a779691118f61050674090405e57d541c5
sofiapantas <sofiasarabia0831@gmail.com>
2024-09-18 02:19:42 +0800

    prep 3 core requirements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        //Console.Write("What is the magic number? ");
+            //string input = Console.ReadLine();
+            //int mNumber = int.Parse(input);
+
+        Random randomGenerator = new Random();
+        int mNumber = randomGenerator.Next(1, 100);
+        string answer = "";
+        while (answer != "You guessed it!")
+        {
+    
+            Console.Write("What is your guess? ");
+            string guessInput = Console.ReadLine();
+            int guess = int.Parse(guessInput);
+    
+    
+            if (guess > mNumber)
+            {
+                answer = "Lower";
+            }
+            else if (guess < mNumber)
+            {
+                answer = "Higher";
+            }
+            else if (guess == mNumber)
+            {
+                answer = "You guessed it!";
+            }
+
+            Console.WriteLine($"{answer}");
+
+        }
+    }   
 }
