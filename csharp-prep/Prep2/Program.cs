2ac9e788c31d81748d9d5ec245d5a04a382ca499
sofiapantas <sofiasarabia0831@gmail.com>
2024-09-18 01:44:53 +0800

    Prep 2 Stretch Challenges

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,76 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+        string letter = "";
+
+        int rem = grade % 10;
+        string sign = "";
+
+
+        if (grade >= 90)
+        {
+            letter = "A";
+            if (rem < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+            if (rem >= 7)
+            {
+                sign = "+";
+            }
+            else if (rem < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+            if (rem >= 7)
+            {
+                sign = "+";
+            }
+            else if (rem < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+            if (rem >= 7)
+            {
+                sign = "+";
+            }
+            else if (rem < 3)
+            {
+                sign = "-";
+            }
+        }
+        else
+        {
+            letter = "F";
+            sign = "";
+        }
+
+
+
+        Console.WriteLine($"Your letter grade is {letter}{sign}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else 
+        {
+               Console.WriteLine("You did not pass. Better luck next time!");
+            }
+
     }
 }
