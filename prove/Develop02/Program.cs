1e7cb2172885959841be87d78d18eccbb24ca5ec
sofiapantas <sofiasarabia0831@gmail.com>
2024-09-28 21:07:58 +0800

    Greeting

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,8 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        Console.WriteLine("Welcome to the Journal Program!");
+        
         Journal theJournal = new Journal();
         Entry anEntry = new Entry();
         anEntry.DisplayEntry();
-    }
+    }   
+
+
 }
