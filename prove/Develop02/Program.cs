a020780b3af18b27629cf557e7a37945f7cffc5b
sofiapantas <sofiasarabia0831@gmail.com>
2024-09-29 10:04:46 +0800

    added prompts to PromptGenerator

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,12 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-
-        Console.WriteLine("Welcome to the Journal Program!");
-        
         Journal theJournal = new Journal();
         Entry anEntry = new Entry();
         anEntry.DisplayEntry();
+
+        Console.WriteLine("Welcome to the Journal Program!");
+        string userInput = Console.ReadLine();
+
+        while (userInput != "5")
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+
+            if (userInput == "1")
+            {
+                
+            }
+            else if (userInput == "2")
+            {
+
+            }
+            else if (userInput == "3")
+            {
+
+            }
+            else if (userInput == "4")
+            {
+
+            }
+            
+        }   
+
     }   
 
 
--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,11 +1,33 @@
 public class PromptGenerator
  {
-    public  List<string>_prompts = new List<string>();
-
-
-    public string GetRandomPrompt()
+    public  List<string>_prompts {get; set;} = new List<string>() 
     {
-        return "";
-    }
+      "What is your biggest dream?",
+        "What are you grateful for today?",
+        "Describe your ideal day",  
+        "What is a positive habit that would really like to cultivate?",
+        "What is something you wish you done today?",
+        "Write about something that inspired you.",
+        "What are you nervous or anxious about today?",
+        "What did you learn today and how can you apply this knowledge?",
+        "What challenges did you face today. How did you overcome them and what did you learn from it?",
+        "What was a moment of joy that you experienced today?",
+        "What was a small but significant detail that you noticed today?",
+        "What could have gone differently?",
+        "Tell me about an old friend you've lost touch with.",
+        "Did something happen to make you sad?",
+        "When was the last time you cried?",
+        "Describe your typical day, from wake to sleep."
+    };
+
+ 
+
+
+    public string GetRandomPrompt() 
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+;    }
     
 }
