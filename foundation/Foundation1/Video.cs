3937349a648fe9f77b066fa564aaad9efecda47a
sofiapantas <sofiasarabia0831@gmail.com>
2024-10-11 16:46:25 +0800

    cut down spacing

--- foundation/Foundation1/Video.cs
+++ foundation/Foundation1/Video.cs
@@ -3,34 +3,28 @@ public class Video {
     private string _author;
     private int _length;
     private List<Comment> _comments;
-
+    
     public Video(string title, string author, int length){
         _title = title;
         _author = author;
         _length = length;
         _comments = new List<Comment>();
     }
-
     public int CountComment(){
         return _comments.Count();
     }
-
     public void AddComment(Comment comment) {
         _comments.Add(comment);
     }
-
     public string GetTitle(){
         return _title;
     }
-
     public string GetAuthor(){
         return _author;
     }
-
     public int GetLength(){
         return _length;
     }
-
     public IEnumerable<Comment> GetComments()
     {
         return _comments.AsReadOnly();
