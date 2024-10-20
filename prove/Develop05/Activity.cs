f3c565d76ad9e658013a93eaa76cf02251e04083
sofiapantas <sofiasarabia0831@gmail.com>
2024-10-20 16:05:05 +0800

    breathing activity

---
+++ prove/Develop05/Activity.cs
@@ -0,0 +1,69 @@
+public class Activity 
+{
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+
+    public Activity(string name, string description, int duration = 0)
+    {
+        _name = name;
+        _description = description;
+        _duration = duration;
+    }
+
+    public void Start()
+    {
+        DisplayStartingMessage();
+
+        Console.WriteLine("Preparing to begin...");
+        ShowSpinner(3);
+    }
+
+    public void SetDuration()
+    {
+        Console.Write("Enter the duration you want to spend doing this activity: ");
+        _duration = int.Parse(Console.ReadLine());
+    }
+
+    public int GetDuration()
+    {
+        return _duration;
+    }
+
+    public void DisplayStartingMessage()
+    {
+        Console.WriteLine($"Welcome to the {_name} Activity");
+        Console.WriteLine($"{_description}");
+        SetDuration();  
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine("Great job, I'm proud of you!");
+        ShowSpinner(3);
+        Console.WriteLine($"Activity Completed: {_name}");
+        Console.WriteLine($"Duration: {_duration} seconds");
+        ShowSpinner(5);
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+        Console.Write("Loading");
+        for (int i = 0; i < seconds; i++)
+        {
+            Console.Write(".");
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine();
+    }
+
+    public void ShowCountdown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.WriteLine($"{i}");
+            System.Threading.Thread.Sleep(1000);
+        }
+        Console.WriteLine();
+    }
+}
---
+++ prove/Develop05/BreathingActivity.cs
@@ -0,0 +1,22 @@
+public class BreathingActivity : Activity
+{
+    public BreathingActivity() : base("Breathing Activity", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.")   
+    {
+    }
+
+    public void Run()
+    {
+        base.Start();
+
+        Console.WriteLine("Let's begin the breathing exercise");
+        int breaths = GetDuration()/5;
+        for (int i = 0; i < breaths; i++)
+        {
+            Console.WriteLine("Breathe in...");
+            ShowCountdown(5);
+            Console.WriteLine("Breathe out...");
+            ShowCountdown(5);
+        }
+        base.DisplayEndingMessage();
+    }
+}
