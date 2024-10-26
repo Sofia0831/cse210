7d3dca1daa56337d195dd899bd475ca5df664552
sofiapantas <sofiasarabia0831@gmail.com>
2024-10-26 23:58:19 +0800

    eternalgoal test

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -7,11 +7,18 @@ class Program
         Console.WriteLine("Hello Develop06 World!");
 
         SimpleGoal goal = new SimpleGoal("Run Marathon", "Complete a full marathon", 1000);
-
         Console.WriteLine(goal.GetStringRepresentation());
-
         goal.RecordEvent();
 
+
+        EternalGoal goal2 = new EternalGoal("Read the Scriptures", "Read the scriptures for at least 10 minutes", 50);
+        Console.WriteLine(goal2.GetStringRepresentation());
+        goal2.RecordEvent();
+
+        Console.WriteLine();
+
         Console.WriteLine(goal.GetStringRepresentation());
+        Console.WriteLine(goal2.GetStringRepresentation());
+
     }
 }
