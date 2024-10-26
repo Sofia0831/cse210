c6c8b56da2692a3d8a7f3aa184f917822c0acc2d
sofiapantas <sofiasarabia0831@gmail.com>
2024-10-26 23:46:49 +0800

    eternalGoal

--- prove/Develop06/EternalGoal.cs
+++ prove/Develop06/EternalGoal.cs
@@ -1,17 +1,14 @@
 public class EternalGoal : Goal
 {
-    private int _countPoints;
+
     public EternalGoal(string name, string description, int points): base(name, description, points)
     {
-        _countPoints = 0;
-
         
     }
 
     public override void RecordEvent()
     {
-        _countPoints++;
-    
+        
     }
 
      public override bool IsComplete()
