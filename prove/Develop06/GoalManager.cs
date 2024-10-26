5f9d3b526e0827ee68ecd0603ed91a9ea38093ff
sofiapantas <sofiasarabia0831@gmail.com>
2024-10-27 01:56:43 +0800

    added loop

--- prove/Develop06/GoalManager.cs
+++ prove/Develop06/GoalManager.cs
@@ -15,42 +15,46 @@ public class GoalManager
         DisplayPLayerInfo();
         Console.WriteLine();
 
-        Console.WriteLine("what would you like to do? ");
-        Console.WriteLine("Menu Options: ");
-        Console.WriteLine("1. Create New Goal");
-        Console.WriteLine("2. List Goals");
-        Console.WriteLine("3. Save Goals");
-        Console.WriteLine("4. Load Goals");
-        Console.WriteLine("5. Record Event");
-        Console.WriteLine("6. Quit");
-        Console.Write("Select an option from the menu (1-6): ");
+        while (true)
+        {
 
-         int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine("What would you like to do? ");
+            Console.WriteLine("Menu Options: ");
+            Console.WriteLine("1. Create New Goal");
+            Console.WriteLine("2. List Goals");
+            Console.WriteLine("3. Save Goals");
+            Console.WriteLine("4. Load Goals");
+            Console.WriteLine("5. Record Event");
+            Console.WriteLine("6. Quit");
+            Console.Write("Select an option from the menu (1-6): ");
 
-            switch (choice)
-            {
-                case 1:
-                    CreateGoal();
-                    break;
-                case 2:
-                    ListGoalNames();
-                    break;
-                case 3:
-                    SaveEvent();
-                    break;
-                case 4:
-                    LoadGoals();
-                    break;
-                case 5:
-                    RecordEvent();
-                    break;
-                case 6:
-                    Environment.Exit(0);
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice.");
-                    break;
-            }
+             int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        CreateGoal();
+                        break;
+                    case 2:
+                        ListGoalDetails();
+                        break;
+                    case 3:
+                        SaveEvent();
+                        break;
+                    case 4:
+                        LoadGoals();
+                        break;
+                    case 5:
+                        RecordEvent();
+                        break;
+                    case 6:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+        }
 
     }
 
--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -6,28 +6,31 @@ class Program
     {
         Console.WriteLine("Hello Develop06 World!");
 
-        SimpleGoal goal = new SimpleGoal("Run Marathon", "Complete a full marathon", 1000);
-        Console.WriteLine(goal.GetStringRepresentation());
-        goal.RecordEvent();
+        // SimpleGoal goal = new SimpleGoal("Run Marathon", "Complete a full marathon", 1000);
+        // Console.WriteLine(goal.GetStringRepresentation());
+        // goal.RecordEvent();
 
 
-        EternalGoal goal2 = new EternalGoal("Read the Scriptures", "Read the scriptures for at least 10 minutes", 50);
-        Console.WriteLine(goal2.GetStringRepresentation());
-        goal2.RecordEvent();
+        // EternalGoal goal2 = new EternalGoal("Read the Scriptures", "Read the scriptures for at least 10 minutes", 50);
+        // Console.WriteLine(goal2.GetStringRepresentation());
+        // goal2.RecordEvent();
 
-        CheckListGoal goal3 = new CheckListGoal("Attend the temple", "Attend and perform any ordinance", 50, 3, 100);
-        Console.WriteLine(goal3.GetStringRepresentation());
-        goal3.RecordEvent();
-        goal3.RecordEvent();
-        goal3.RecordEvent();
+        // CheckListGoal goal3 = new CheckListGoal("Attend the temple", "Attend and perform any ordinance", 50, 3, 100);
+        // Console.WriteLine(goal3.GetStringRepresentation());
+        // goal3.RecordEvent();
+        // goal3.RecordEvent();
+        // goal3.RecordEvent();
 
-        Console.WriteLine();
+        // Console.WriteLine();
 
-        Console.WriteLine(goal.GetStringRepresentation());
-        Console.WriteLine(goal2.GetStringRepresentation());
-        Console.WriteLine(goal3.GetStringRepresentation());
+        // Console.WriteLine(goal.GetStringRepresentation());
+        // Console.WriteLine(goal2.GetStringRepresentation());
+        // Console.WriteLine(goal3.GetStringRepresentation());
 
-        Console.WriteLine(goal.GetDetailString());
+        // Console.WriteLine(goal.GetDetailString());
+
+        GoalManager manager = new GoalManager();
+        manager.Start();
 
     }
 }
