b6c1327c620d3f168c883d517c3e4e885a70fec5
sofiapantas <sofiasarabia0831@gmail.com>
2024-10-26 23:52:44 +0800

    added eventCount

--- prove/Develop06/EternalGoal.cs
+++ prove/Develop06/EternalGoal.cs
@@ -1,14 +1,15 @@
 public class EternalGoal : Goal
 {
+    private int _eventCount;
 
     public EternalGoal(string name, string description, int points): base(name, description, points)
     {
-        
+        _eventCount = 0;
     }
 
     public override void RecordEvent()
     {
-        
+        _eventCount++;
     }
 
      public override bool IsComplete()
@@ -19,6 +20,6 @@ public class EternalGoal : Goal
 
     public override string GetStringRepresentation()
     {
-        return $"[ ] {GetDetailString()}";
+        return $"[ ] {GetDetailString()} (Recorded {_eventCount} times)";
     }
 }
