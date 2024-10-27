6bc5025c3d9321e24bfa0c11a231592c3d793905
sofiapantas <sofiasarabia0831@gmail.com>
2024-10-27 17:28:22 +0800

    spacing

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -4,30 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop06 World!");
-
-        // SimpleGoal goal = new SimpleGoal("Run Marathon", "Complete a full marathon", 1000);
-        // Console.WriteLine(goal.GetStringRepresentation());
-        // goal.RecordEvent();
-
-
-        // EternalGoal goal2 = new EternalGoal("Read the Scriptures", "Read the scriptures for at least 10 minutes", 50);
-        // Console.WriteLine(goal2.GetStringRepresentation());
-        // goal2.RecordEvent();
-
-        // CheckListGoal goal3 = new CheckListGoal("Attend the temple", "Attend and perform any ordinance", 50, 3, 100);
-        // Console.WriteLine(goal3.GetStringRepresentation());
-        // goal3.RecordEvent();
-        // goal3.RecordEvent();
-        // goal3.RecordEvent();
-
-        // Console.WriteLine();
-
-        // Console.WriteLine(goal.GetStringRepresentation());
-        // Console.WriteLine(goal2.GetStringRepresentation());
-        // Console.WriteLine(goal3.GetStringRepresentation());
-
-        // Console.WriteLine(goal.GetDetailString());
+        Console.WriteLine();
 
         GoalManager manager = new GoalManager();
         manager.Start();
